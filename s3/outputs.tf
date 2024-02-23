b1c5c9c894eb18c82b5a630d431417172cda342d
Marcus Nestorius Austria <marcus.austria@coherent.global>
2024-02-23 14:24:28 +0800

    Output

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,9 +1,7 @@
 output "s3_bucket_id" {
-  description = "The name of the bucket."
-  value       = try(aws_s3_bucket_policy.this[0].id, aws_s3_bucket.this[0].id, "")
+  value = aws_s3_bucket.s3_bucket.id
 }
 
 output "s3_bucket_arn" {
-  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = try(aws_s3_bucket.this[0].arn, "")
+  value = aws_s3_bucket.s3_bucket.arn
 }
