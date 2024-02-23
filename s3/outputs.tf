d0d9c403d7399cb92d52949232829ec1d6acdb4f
Marcus Nestorius Austria <marcus.austria@coherent.global>
2024-02-23 14:31:04 +0800

    Output fix

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_arn" {
-  value = s3-bucket.s3_bucket_arn
+  value = module.s3-bucket.s3_bucket_arn
 }
 
 output "s3_bucket_id" {
-  value = s3-bucket.s3_bucket_id
+  value = module.s3-bucket.s3_bucket_id
 }
