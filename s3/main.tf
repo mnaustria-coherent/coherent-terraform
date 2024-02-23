1259489dc31ed136e9d2d2494543bade0b4ebb38
Marcus Nestorius Austria <marcus.austria@coherent.global>
2024-02-23 14:46:41 +0800

    add input

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,5 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.0"
-
-  name = var.name
-  region = var.region
-  prefix = var.prefix
+  bucket = "${var.prefix}-${var.name}"
 }
